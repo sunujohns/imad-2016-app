32023c48b20d4a25eff3ddda5d3f429ac70e5b70
sunujohns <johns.sunu@gmail.com>
2016-10-01 20:53:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName',function(req,res){
     var articleName = req.params.articleName;
-    res.send(createTemplate(articleName));
+    res.send(createTemplate(articles[articleName]));
 });
 
 
