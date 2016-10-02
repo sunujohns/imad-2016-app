da154e438604e23feaf197acbfccb77daf8dfad2
sunujohns <johns.sunu@gmail.com>
2016-10-02 22:02:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,8 +98,8 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 var names = [];
-app.get('/submit-name/:name',function(req,res){
-    var name = req.params.name;
+app.get('/submit-name',function(req,res){
+    var name = req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
