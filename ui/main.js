8f0280c60cae9833f9e0d2860ca596733d0da8c3
sunujohns <johns.sunu@gmail.com>
2016-10-02 21:42:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,9 +23,9 @@ button.onclick =function(){
 
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
-var submit = document.getElementById('submit_btn')
+var submit = document.getElementById('submit_btn');
 
-submit.onclick() =  function(){
+submit.onclick =  function(){
     
     
     var names = ['name1','name2','name3','name4'];
@@ -33,5 +33,6 @@ submit.onclick() =  function(){
     for(var i=0;i<names.length;i++){
         list += '<li>'+names[i]+'<li>';
     }
-    var ul =document.getElementById('namelist')
-}
+    var ul =document.getElementById('namelist');
+    ul.innerHTML = list;
+};
