5e4f3842abab45e1a554c96a7b01a1cac8d8b20b
sunujohns <johns.sunu@gmail.com>
2016-10-02 12:16:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 
 var element =  document.getElementById("main-text");
 element.innerHTML = "New Value";
+
+//move image
+
+var img  = document.getElementById("madi");
+img.onclick = function() {
+    img.style.marginleft = '100px';
+};
