377f5a2a4fa389e5a4d8b3c7c1a9182223b9a76e
sunujohns <johns.sunu@gmail.com>
2016-10-02 14:03:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,3 +16,14 @@ img.onclick = function() {
     var interval = setInterval(moveRight , 100);
     
 };
+
+
+var button = document.getElementById("button");
+
+button.onclick =function(){
+  
+  
+  counter= counter+1;
+  var span = document.getElementById("count");
+  span.innerHTML = countet.toString();
+};
