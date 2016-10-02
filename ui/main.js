b09dda3794d6273ed0a9d9e2308ae745d549216a
sunujohns <johns.sunu@gmail.com>
2016-10-02 20:47:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,22 @@
 
 
 var button = document.getElementById("counter");
-var counter =0;
 button.onclick =function(){
   
   var request = new XMLHttpRequest();
   
   request.onreadystatechange = function(){
-      if(request.readystate === XMLHttpRequest.)
+      if(request.readystate === XMLHttpRequest.DONE){
+          if(reques.status == 200){
+              var counter = request.responseText;
+              var span = document.getElementById("count");
+              span.innerHTML = counter.toString();            
+          }
+      }
   };
+  //make request
   
-  counter= counter+1;
-  var span = document.getElementById("count");
-  span.innerHTML = counter.toString();
+  request.oprn('GET','http://http://sunujohns.imad.hasura-app.io/counter',true);
+  request.send(true);
+  
 };
