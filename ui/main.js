2839741090f5879fe87f3d47a5e9baf1d50f0077
sunujohns <johns.sunu@gmail.com>
2016-10-02 14:23:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,11 @@ var button = document.getElementById("counter");
 var counter =0;
 button.onclick =function(){
   
+  var request = new XMLHttpRequest();
+  
+  request.onreadystatechange = function(){
+      if(request.readystate === XMLHttpRequest.)
+  };
   
   counter= counter+1;
   var span = document.getElementById("count");
